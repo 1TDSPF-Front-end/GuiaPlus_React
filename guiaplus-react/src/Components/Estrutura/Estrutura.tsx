a29ce9c058c55d050d682fa315ea7922cefa9437
guilhermesolagarcia <guilhermesolagarcia31@gmail.com>
2025-09-18 17:54:29 -0300

    Ajustando o css com rotas.

--- guiaplus-react/src/Components/Estrutura/Estrutura.tsx
+++ guiaplus-react/src/Components/Estrutura/Estrutura.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatches } from 'react-router-dom';
 import Cabecalho from '../Cabecalho/Cabecalho';
 import Rodape from '../Rodape/Rodape';
 
 function Estrutura() {
+  const matches = useMatches();
+  const tituloDaRota = (matches[matches.length - 1]?.handle as { titulo: string } | undefined)?.titulo || 'Título Padrão';
+  
   return (
     <div className="container">
-      <Cabecalho titulo="Título da página" />
+      <Cabecalho titulo={tituloDaRota} />
       <Outlet />
       <Rodape />
     </div>
--- guiaplus-react/src/routes.tsx
+++ guiaplus-react/src/routes.tsx
@@ -18,22 +18,27 @@ const router = createBrowserRouter([
       {
         path: '/pagina-inicial',
         element: <PaginaInicial />,
+        handle: { titulo: 'Página Inicial' },
       },
       {
         path: '/faq',
         element: <FAQ />,
+        handle: { titulo: 'Perguntas Frequentes' },
       },
       {
         path: '/integrantes',
         element: <Integrantes />,
+        handle: { titulo: 'Integrantes' },
       },
       {
         path: '/contato',
         element: <Contato />,
+        handle: { titulo: 'Contato' },
       },
       {
         path: '/tutoriais',
         element: <Tutoriais />,
+        handle: { titulo: 'Tutoriais em Vídeo' },
       },
     ],
   },
